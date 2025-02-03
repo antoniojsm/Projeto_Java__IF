97aa164cdcd6feaa769e7abb857638732d00ba30
Antonio José dos santos menezes <antoniojoseyt341@gmail.com>
2025-02-03 19:23:52 -0300

    primeiro commit projeto app

---
+++ Projeto_App/src/main/java/com/mycompany/projeto_app/Projeto_App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_app;
+
+/**
+ *
+ * @author anton
+ */
+public class Projeto_App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
