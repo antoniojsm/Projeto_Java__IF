60e6b0a58ed468d2d9436bf63ad3b6ed51f164fc
Antonio José dos santos menezes <antoniojoseyt341@gmail.com>
2025-02-06 14:27:51 -0300

    criação tabela livro

--- BD usuario biblioteca if.sql
+++ BD usuario biblioteca if.sql
@@ -4,3 +4,9 @@ CREATE TABLE usuario_biblioteca(
 id_usuario INT PRIMARY KEY AUTO_INCREMENT,
 nome_usuario VARCHAR (45) NOT NULL,
 senha VARCHAR (45) NOT NULL);
+
+CREATE TABLE livros(
+id_livro INT PRIMARY KEY AUTO_INCREMENT,
+titulo_livro VARCHAR (45) NOT NULL,
+autor_livro VARCHAR (45) NOT NULL,
+disponibilidade BOOLEAN NOT NULL,
