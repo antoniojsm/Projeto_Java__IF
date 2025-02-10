6ab13d7532a0d24454d2c92a39de2d50f190db22
Antonio José dos santos menezes <antoniojoseyt341@gmail.com>
2025-02-10 11:25:50 -0300

    update projeto biblioteca

--- Projeto_App/src/main/java/com/mycompany/projeto_app/projeto/factory/ConexãoMySQL.java
+++ Projeto_App/src/main/java/com/mycompany/projeto_app/projeto/factory/ConexãoMySQL.java
@@ -2,15 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.projeto_app.projeto.dao;
+package com.mycompany.projeto_app.projeto.factory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-
-
 
 /**
  *
