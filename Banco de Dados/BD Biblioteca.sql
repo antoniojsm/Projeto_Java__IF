254bc676022dc0bf6c0ebc6dc546584c8770da84
Antonio José dos santos menezes <antoniojoseyt341@gmail.com>
2025-02-13 16:12:37 -0300

    criação da pasta banco de dados

---
+++ Banco de Dados/BD Biblioteca.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE biblioteca;
+USE biblioteca;
+
+CREATE TABLE usuario(
+id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+usuario VARCHAR(45) NOT NULL,
+senha VARCHAR (45) NOT NULL);
+
+CREATE TABLE cadastrolivros(
+id_livro INT AUTO_INCREMENT PRIMARY KEY,
+titulo_livro VARCHAR (45) NOT NULL,
+autor_livro VARCHAR (45) NOT NULL, 
+disponibilidade INT NOT NULL);
+
+INSERT INTO usuario VALUES 
+(NULL, 'Antonio Jose', 12345)
